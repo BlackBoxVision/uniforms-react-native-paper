1ed1851cf93e7dd08d63473943333d3a8170f11d
JonatanSalas <jonatan.salas@blackbox-vision.tech>
2022-04-07 23:29:18 -0300

    chore: minor change in example

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Alert } from "react-native"
 import { AutoForm } from '@blackbox-vision/uniforms-react-native-paper';
 
 import { bridge as schema } from './schema/signup';
@@ -7,7 +8,9 @@ export default function App() {
   return (
     <AutoForm
       schema={schema}
-      onSubmit={(model: any) => alert(JSON.stringify(model, null, 2))}
+      onSubmit={(model: any) => {
+        Alert.alert(JSON.stringify(model, null, 2))
+      }}
     />
   );
 }
